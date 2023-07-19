78a9054efc7360f2fe796696ba3108d1c6a86b4b
duongpt582 <tduong.hust02@gmail.com>
2023-07-19 10:02:47 +0700

    fix build.gradle/app

--- app/src/main/java/com/pdftools/noteappkotlinmvvm/NoteApp.kt
+++ app/src/main/java/com/pdftools/noteappkotlinmvvm/NoteApp.kt
@@ -2,6 +2,7 @@ package com.pdftools.noteappkotlinmvvm
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot
 
-@HiltAndroidApp
+@HiltAndroidApp()
 class NoteApp: Application()
